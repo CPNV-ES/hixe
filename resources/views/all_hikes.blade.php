09604a2af52abfca23d7bb199f63bd2ba626e48a
MIGEWANT Dylan <Dylan.MIGEWANT@cpnv.ch>
2019-10-31 08:18:22 +0100

    For #1: Page that displays all hikes

---
+++ resources/views/all_hikes.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.base')
+
+@section('body-content')
+  <table>
+    <tr>
+      <th>Date</th>
+      <th>Destination</th>
+      <th>Guide</th>
+      <th>Inscrits</th>
+      <th>Maximum</th>
+      <th>État</th>
+    </tr>
+
+    @foreach ($hikes as $hike)
+      <tr>
+        <td>{{ $hike->meeting_date }}</td>
+        <td>{{ implode(', ', $hike->destinations()->pluck('location')->toArray()) }}</td>
+        <td>{{ implode(', ', $hike->guides()->pluck('firstname')->toArray()) }}</td>
+        <td>{{ count($hike->users()->get()) }}</td>
+        <td>{{ $hike->max_num_participants }}</td>
+        <td>{{ $hike->state->name }}</td>
+      </tr>
+    @endforeach
+</table>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base')
+
+@section('body-content')
+  <div class="title m-b-md">
+    Hixe
+  </div>
+  <div class="links">
+    Le site d'organisation de courses en montagne du CAS Valais
+  </div>
+@endsection
