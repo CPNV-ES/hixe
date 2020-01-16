7b4f3b29d71ee6af9b36d015d63962d677e7da95
Zaid <zaid.schouwey@gmail.com>
2020-01-16 15:48:00 +0100

    Fix merge feature

--- resources/views/all_hikes.blade.php
+++ resources/views/all_hikes.blade.php
@@ -3,25 +3,30 @@
 @section('title', 'Toutes les courses')
 
 @section('body-content')
-<table>
-  <tr>
-    <th>Date</th>
-    <th>Destination</th>
-    <th>Guide</th>
-    <th>Inscrits</th>
-    <th>Maximum</th>
-    <th>État</th>
-  </tr>
+<table class="table">
+    <thead class="thead-dark">
+        <tr>
+            <th scope="col">Date</th>
+            <th scope="col">Destination</th>
+            <th scope="col">Guide</th>
+            <th scope="col">Inscrits</th>
+            <th scope="col">Maximum</th>
+            <th scope="col">État</th>
+        </tr>
+    </thead>
 
-  @foreach ($hikes as $hike)
-  <tr>
-    <td>{{ $hike->meeting_date }}</td>
-    <td>{{ implode(', ', $hike->destinations()->pluck('location')->toArray()) }}</td>
-    <td>{{ implode(', ', $hike->guides()->pluck('firstname')->toArray()) }}</td>
-    <td>{{ count($hike->users()->get()) }}</td>
-    <td>{{ $hike->max_num_participants }}</td>
-    <td>{{ $hike->state->name }}</td>
-  </tr>
-  @endforeach
+    <tbody>
+
+    @foreach ($hikes as $hike)
+    <tr>
+        <th scope="row">{{ $hike->meeting_date }}</td>
+        <td>{{ implode(', ', $hike->destinations()->pluck('location')->toArray()) }}</td>
+        <td>{{ implode(', ', $hike->guides()->pluck('firstname')->toArray()) }}</td>
+        <td>{{ count($hike->users()->get()) }}</td>
+        <td>{{ $hike->max_num_participants }}</td>
+        <td>{{ $hike->states->name}}</td>
+    </tr>
+    @endforeach
+</tbody>
 </table>
 @endsection
