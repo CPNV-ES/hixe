c2130806fc90cbd8a70c4e4b873ddcff92c801c3
Zaid <zaid.schouwey@gmail.com>
2020-01-30 14:29:00 +0100

    Fix merge conflicts

--- app/Models/Hike.php
+++ app/Models/Hike.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hike extends Model
 {
-<<<<<<< HEAD
   public $timestamps = false;
 
   public function users()
@@ -28,27 +27,4 @@ class Hike extends Model
   {
     return $this->belongsTo(State::class);
   }
-=======
-    public $timestamps = false;
-
-    public function users()
-    {
-        return $this->belongsToMany(User::class)->withPivot('role_id');
-    }
-
-    public function guides()
-    {
-        return $this->users()->where('role_id', 1);
-    }
-
-    public function destinations()
-    {
-        return $this->belongsToMany(Destination::class);
-    }
-
-    public function states()
-    {
-        return $this->belongsTo(State::class);
-    }
->>>>>>> 08385abdf6f863c9374408941e50c2e85bc5187b
 }
