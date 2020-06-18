c291595c21e890165dac02ce15ae05bf893f1f60
Zaid <zaid.schouwey@gmail.com>
2020-06-18 14:06:12 +0200

    Fix Calendar

--- resources/views/hikecalendar/index.blade.php
+++ resources/views/hikecalendar/index.blade.php
@@ -4,16 +4,16 @@
 @section('title', 'Calendrier')
 
 @section('body-content')
-<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.css' />
+{{-- <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.css' /> --}}
 
 
 <div class="container">
-    <h1>Calendar</h1>
+    <h1>Calendar testestestesssst</h1>
 <div id='calendar'></div>
 </div>
-<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+{{-- <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.js'></script> --}}
 <style>
     .fc-day:hover{
         background:lightblue;
@@ -21,7 +21,7 @@
     }
 
  </style>
-<script>
+{{-- <script>
     $(document).ready(function() {
         // page is now ready, initialize the calendar...
         $('#calendar').fullCalendar({
@@ -52,5 +52,5 @@
 
         })
     });
-</script>
+</script> --}}
 @endsection
