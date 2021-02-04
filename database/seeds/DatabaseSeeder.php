6e34c4647b1be62fefbae90077558285856b5867
Mathieu <mathieu.burnat@cpnv.ch>
2021-02-04 09:16:25 +0100

    Complet seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     $this->call([UsersTableSeeder::class]);
     $this->call([RolesTableSeeder::class]);
     $this->call([StatesTableSeeder::class]);
-    $this->call([EquipmentTableSeeder::class]);
     $this->call([DestinationsTableSeeder::class]);
     $this->call([TrainingsTableSeeder::class]);
   }
