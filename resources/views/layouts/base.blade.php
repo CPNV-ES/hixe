38ad686b297f70858abf07a645d2d52208cc0f02
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2021-03-04 09:17:09 +0100

    Remove link to CDN version of jquery

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -19,8 +19,6 @@
     <script src="{{ asset('/lib/jquery/jquery.min.js')}}"></script>
     <script src="{{ asset('/lib/bootstrap/bootstrap.min.js') }}"></script>
     <script src="{{ asset('/js/app.js')}}"></script>
-
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 
 <style>
