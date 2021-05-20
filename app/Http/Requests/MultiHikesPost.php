27a539d86f1186fbafe254c7b740dff5d372bb6d
PEREIRA Gabriel <Gabriel.PEREIRA@cpnv.ch>
2021-05-20 10:06:58 +0200

    Put back the old rules

--- app/Http/Requests/MultiHikesPost.php
+++ app/Http/Requests/MultiHikesPost.php
@@ -25,38 +25,20 @@ class MultiHikesPost  extends FormRequest
     public function rules()
     {
         return [
-            'name'              =>      'required|max:100',
-            'meetingLocation'   =>      'required|max:100',
-            //'meetingDate'       =>      'required|date|date_format:d.m.Y',
-            //'hikeDate'          =>      'required|date|date_format:d.m.Y|after_or_equal:meetingDate',
-            //'start'             =>      'required|date|date_format:H:i',
-            //'finish'            =>      'required|date|date_format:H:i',
-            'min'               =>      'numeric',
-            'max'               =>      'numeric',
-            'denivele'          =>      'required|numeric' ,
-            'difficulty'        =>      'required|numeric',
+            'name'              => 'required',
+            'meetingLocation'   => 'required',
+            'meetingDate'       => 'required',
+            'hikeDate'          => 'required',
+            'start'             => 'required',
+            'finish'            => 'required',
+            'difficulty'        => 'required',
+            'denivele'          => 'required'
         ];
     }
 
     public function messages(){
         return [
-            'name.required' => 'Le champ nom de la course est obligatoire',
-            'meetingLocation.required' => 'Le champ nom du lieu de rendez-vous est obligatoire',
-            'meetingDate.required' => 'Le champ date du rendez-vous est obligatoire',
-            'meetingDate.date_format' => 'Écrivez la date du rendez-vous au format jour/mois/année',
-            'hikeDate.required' => 'Le champ date de fin est obligatoire',
-            'hikeDate.date_format' => 'Écrivez la date au format mois/jour/année',
-            'hikeDate.after_or_equal' => 'La date de fin doit être après la date du rendez-vous',
-            'start.required' => 'Le champ heure du rendez-vous est obligatoire',
-            'start.date_format' => 'Écrivez l\'heure du rendez-vous au format jour/mois/année',
-            'finish.required' => 'Le champ heure de fin est obligatoire',
-            'finish.date_format' => 'Écrivez l\'heure de fin au format mois/jour/année',
-            'min.numeric' => 'Dans le champ minimum écrivez uniquement des chiffres',
-            'max.numeric' => 'Dans le champ minimum écrivez uniquement des chiffres',
-            'denivele.required' => 'Le champ denivelé est obligatoire',
-            'denivele.numeric' => 'Dans le champ denivelé écrivez uniquement des chiffres',
-            'difficulty.required' => 'Le champ denivelé est obligatoire',
-            'difficulty.numeric' => 'Dans le champ difficulty écrivez uniquement des chiffres',
+            'name.required' => 'Le Nom est obligatoire',
         ];
     }
 }
