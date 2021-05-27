936ba284dddc2d2dad26dc99793c4af0a6afe5b1
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2021-05-27 08:39:56 +0200

    Move SearchController search method to UserController class

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -100,4 +100,24 @@ class UserController extends Controller
     {
         //
     }
+
+    public function search(Request $request) {
+        $query = $request->query('q');
+
+        $needles = explode(' ', $query);
+
+        $q = User::query();
+        
+        if ($needles) {
+            foreach($needles as $needle) {
+                $q = $q->where('firstname', 'like', "%{$needle}%")->orWhere('lastname', 'like', "%{$needle}%");
+            }
+        } else {
+            $q->where('firstname', 'like', "%{$query}%")->orWhere('lastname', 'like', "%{$query}%");
+        }
+        
+        $users = $q->get()->all();
+
+        return response()->json($users);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\SearchController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users/search', [SearchController::class, 'index']);
+/**
+ * TODO: Apply authentication middleware
+ */
+Route::get('/users/search', [UserController::class, 'search']);
