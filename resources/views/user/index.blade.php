b050b7f5d1107da515f631302ce75f6d94e9df7e
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-06-03 10:43:17 +0200

    Create the view

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,5 +3,26 @@
 @section('title', 'Users')
 
 @section('body-content')
-    <h2> Hello there ! </h2>
+    <div class="container-fluid mt-4 table-responsive">
+        <table id="hikesTable" class="table mt-3">
+            <thead>
+                <tr>
+                    <th scope="col">Firstname</th>
+                    <th scope="col">lastname</th>
+                    <th scope="col">Last updated</th>
+                    <th scope="col">Role</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td> </td>    
+                        <td> </td>    
+                        <td> </td>    
+                        <td> </td>    
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
