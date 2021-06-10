6b919eb09235aefff6bf79ef09e0a1c65b83014e
Quentin Aellen <quentin.aellen@cpnv.ch>
2021-06-10 11:12:16 +0200

    fix migrations for deployment

--- database/migrations/2021_02_09_094610_add_role_to_users_table.php
+++ database/migrations/2021_02_09_094610_add_role_to_users_table.php
@@ -14,7 +14,7 @@ class AddRoleToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedInteger('role_id');
+            $table->unsignedInteger('role_id')->default(1);
         });
     }
 
