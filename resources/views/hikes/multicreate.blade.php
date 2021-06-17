9e2b443d3d2bcb51d45739d7f57911389a71f473
PEREIRA Gabriel <Gabriel.PEREIRA@cpnv.ch>
2021-06-17 11:34:40 +0200

    Change the text of the model button

--- resources/views/hikes/multicreate.blade.php
+++ resources/views/hikes/multicreate.blade.php
@@ -15,7 +15,7 @@
                         <p><em>Champs obligatoire*</em></p>
                         <div class="row footer ">
                             <div class="col-md-12 pr-1 d-flex justify-content-end" style="padding-bottom: 15px;">
-                                <a href="{{route('import.export')}}" class="btn btn-outline-secondary">Model .csv</a>
+                                <a href="{{route('import.export')}}" class="btn btn-outline-secondary">Modèle .csv</a>
                                 <form method="POST" action="{{ route('import.store') }}" enctype="multipart/form-data">
                                     @csrf
                                     <div class="input-group">
