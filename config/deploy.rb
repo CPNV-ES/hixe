a74de9e944eab273c410a4e1d971fa400170691a
MisterX25 <Xavier.Carrel@cpnv.ch>
2021-09-08 16:24:14 +0200

    Lock capistrano version low

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.16.0"
+lock "~> 3.14.0"
 
 set :application, "my_app_name"
 set :repo_url, "git@example.com:me/my_repo.git"
